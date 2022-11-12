46ceb3a1ed657200e0a8321d24cd02f62aea0529
juanluis2021 <jsanchezpizarro@cifpfbmoll.eu>
2022-11-12 16:42:54 +0100

    First Commit

---
+++ ejercicio2pract3.py
@@ -0,0 +1,12 @@
+
+
+
+
+kilometros = float (input("introduce los Kilometros recorridos :"))
+Velocidad = float (input("introduce el tiempo en horas del recorrido :"))
+
+media = kilometros / Velocidad
+
+print ( "la velocidad media km/h :" + str (media))
+
+print ( "la velocidad media km/h :",media)
