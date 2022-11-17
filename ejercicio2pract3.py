19f7f296ff4721c209362822cead6097dda26f82
juanluis2021 <jsanchezpizarro@cifpfbmoll.eu>
2022-11-17 20:25:39 +0100

    practica git 2

--- ejercicio2pract3.py
+++ ejercicio2pract3.py
@@ -2,7 +2,7 @@
 
 
 
-kilometros = float (input("introduce los Kilometros recorridos :"))
+kilometros = float (input("introduce los Kilometros recorridosss :"))
 Velocidad = float (input("introduce el tiempo en horas del recorrido :"))
 
 media = kilometros / Velocidad
